1f418cac0f59d01561e7cd5d95b267fb6cb5e3bb
sibampaul <sibampaul@gmail.com>
2019-04-27 20:34:19 +0530

    Working fine

--- Gussing_game.py
+++ Gussing_game.py
@@ -3,18 +3,20 @@ from random import randint
 
 ran = random.randint(1,10)
 
-while True:
-    guess = int(input("Enter your guess between 1 to 10 : "))
 
-    if guess > ran:
-        print("Guess is large : ")
-    elif guess < ran:
-        print(" Guess is small : ")
-    elif guess == ran:
-        print("Congratulation you have chose the right guess !!! ")
-        rep = input("Do you want to continue Y/N :")
-        if rep == "Y" or rep == "y":
-            from random import randint
-            ran=random.randint
-        else:
-            break
+while True:
+  guess = int(input("Guess a number Between 1 to 10 : "))
+  if guess >10 :
+    print("wrong Guess Please chose bet")
+  elif guess > ran:
+    print("guess is large")
+  elif guess < ran:
+    print("guess is small")
+  elif guess == ran:
+    print("congratulation your guess is right !!!")
+    cont = input("Do you want to continue Y/N ?? : ")
+    if cont == "Y" or cont == "y":
+      from random import randint
+      ran = random.randint(1,10)
+    else:
+      break
